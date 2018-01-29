6a3dd117b7549c24fcd0d180c66343ae3745383b
Steve <steve.liushihao@gmail.com>
2018-01-28 23:05:58 -0700

    terminate process then wait

--- tornado/test/autoreload_test.py
+++ tornado/test/autoreload_test.py
@@ -107,6 +107,7 @@ for i in range(50):
         # Once the autoreload process is done, we kill the touch process
         autoreload_proc.wait()
         touch_proc.terminate()
+        touch_proc.wait()
 
         out = autoreload_proc.communicate()[0]
         self.assertEqual((str([os.path.join(os.path.dirname(os.path.abspath(tornado.autoreload.__file__)),
